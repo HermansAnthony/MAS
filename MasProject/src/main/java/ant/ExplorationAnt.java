a56892d9b70ddbdacbed591bb3a848a7b3646451
FedericoQuin <federico-quin@hotmail.com>
2018-05-20 17:05:52 +0200

    Partial implementation of ants to charging point

--- MasProject/src/main/java/ant/ExplorationAnt.java
+++ MasProject/src/main/java/ant/ExplorationAnt.java
@@ -3,7 +3,23 @@ package ant;
 import pdp.Drone;
 
 public class ExplorationAnt extends Ant {
-    public ExplorationAnt(Drone drone) {
+    public AntDestination destination;
+    private double chargingPointOccupation;
+
+    public ExplorationAnt(Drone drone, AntDestination destination) {
         super(drone);
+        this.destination = destination;
+    }
+
+    public void setChargingPointOccupation(double chargingPointOccupation) {
+        this.chargingPointOccupation = chargingPointOccupation;
+    }
+
+    public double getChargingPointOccupation() {
+        return chargingPointOccupation;
+    }
+
+    public enum AntDestination {
+        Order, ChargingPoint;
     }
 }
