3dfcee870f892771c05ada4a6ec6f35e5936432c
babbel056 <hermans.anthony@hotmail.com>
2018-06-10 21:26:25 +0200

    minor change to processing script

--- processResults.py
+++ processResults.py
@@ -2,22 +2,24 @@ from os import listdir
 import re
 import datetime
 import time
+import sys
 
 # Dictionary containing all the results
 results = {
-    "lwCharge": 0,
-    "hwCharge" : 0,
+    "lwCharge": 0.0,
+    "hwCharge" : 0.0,
     "DeliveryTime" :0,
     "OverdueTime": 0,
     "DeliveredOrders": 0,
     "OverdueOrders": 0,
-    "OrderPercentage": 0
+    "OrderPercentage": 0.0
     }
 
-def processPercentage(data):
-    if len(data) == 1:
-        results["OrderPercentage"] += float(data[0].replace(',','.'))
-        return
+def processOrderPercentage(data):
+    results["OrderPercentage"] += float(data[0].replace(',','.'))
+    return
+	
+def processChargePercentage(data):
     for index,element in enumerate(data):
         if element == "NaN": continue
         if index == 0: results["lwCharge"] += float(element.replace(',','.'))
@@ -38,12 +40,12 @@ def processFile(file):
     for index,line in enumerate(lines):
         if index == 0 or index == 1: continue
         data = [s for s in re.findall(r'NaN|-?\d+\,?\d*', line)] # Contains all the numberic data of the line
-        if index == 2: processPercentage(data)
+        if index == 2: processChargePercentage(data)
         if index == 3: processTime(data, "DeliveryTime")
         if index == 4: processTime(data,  "OverdueTime")
         if index == 5: results["DeliveredOrders"] += int(data[0])
         if index == 6: results["OverdueOrders"] += int(data[0])
-        if index == 7: processPercentage(data)
+        if index == 7: processOrderPercentage(data)
 
 def outputResults():
     for key in results.keys():
@@ -52,10 +54,12 @@ def outputResults():
             continue
         print(key + ': ' + str(results[key]))
 
-if __name__ == '__main__':
-    print("Processing results")
-    fileNames = ["experiments/" + name for name in listdir("experiments")]
+if __name__ == '__main__': 
+    directory = "experiments"
+    if (len(sys.argv) >= 2): directory = sys.argv[1]
+    fileNames = [directory+"/" + name for name in listdir(directory)]
     numberOfFiles = len(fileNames)
+    print("Processing results for the {} directory (containing {} files)".format(directory, numberOfFiles))
     for fileName in fileNames:
         file = open(fileName, 'r')
         processFile(file)
